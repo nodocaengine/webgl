65416a32b07bbe2f09b5e248344bb0473ae5061a
nodoca <nodocaengine@gmail.com>
2017-06-12 01:21:52 +0900

    🎉

---
+++ script.js
@@ -0,0 +1,14 @@
+onload = () => {
+  console.log('setup');
+
+  let c = document.getElementById('canvas');
+  
+  c.width = 500;
+  c.height = 300;
+  
+  let gl = c.getContext('webgl') || c.getContext('experimental-webgl');
+  
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+};
+
